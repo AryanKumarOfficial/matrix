a2b3cdd7ee0893daccdfefd690cf6afe3c53637d
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-18 12:50:14 +0530

    integrated contact api

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import {Outlet} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 
 function App() {
-    return (
-        <>
-            <Navbar/>
-            <Outlet/>
-            <Footer/>
-        </>
-    );
+    return (<>
+        <Navbar/>
+        <Toaster
+            position={'top-center'}
+            reverseOrder={false}
+        />
+        <Outlet/>
+        <Footer/>
+    </>);
 }
 
 export default App;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,10 @@
 import React, {useState} from "react";
 import "../styles/contact.css";
+import toast from "react-hot-toast";
 
 const Contact = () => {
     const [formData, setFormData] = useState({
-        name: "",
-        email: "",
-        message: ""
+        name: "", email: "", message: ""
     });
 
     const handleChange = (e) => {
@@ -13,58 +12,64 @@ const Contact = () => {
         setFormData({...formData, [name]: value});
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Handle form submission logic here (e.g., send data to an API)
-        console.log("Form submitted:", formData);
-        alert("Thank you for contacting us!");
+        const req = await fetch(`${process.env.REACT_APP_API_HOST}/api/contact/add`, {
+            method: "POST", headers: {
+                "Content-Type": "application/json",
+            }, body: JSON.stringify(formData)
+        })
+
+        const res = await req.json();
+        if (res.success) {
+            toast.success(res.message)
+        } else {
+            toast.error(res.message)
+        }
+
         setFormData({
-            name: "",
-            email: "",
-            message: ""
+            name: "", email: "", message: ""
         });
     };
 
-    return (
-        <section className="contact">
-            <h1>Contact Us</h1>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        value={formData.message}
-                        onChange={handleChange}
-                        required
-                    ></textarea>
-                </div>
-                <button type="submit">Submit</button>
-            </form>
-        </section>
-    );
+    return (<section className="contact">
+        <h1>Contact Us</h1>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    required
+                />
+            </div>
+            <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea
+                    id="message"
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                    required
+                ></textarea>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </section>);
 };
 
 export default Contact;
