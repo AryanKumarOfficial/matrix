81254ca95afc6bb03de70309e850adcd42bd402e
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-18 13:37:05 +0530

    added env

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 require("dotenv").config();
 const express = require('express');
 const connectDB = require('./database/middleware/Mongoose');
-const cors = require("cors")
+const cors = require("cors");
+
 const app = express();
+
+// Middleware to parse JSON requests and handle CORS
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.get("/", async (req, res) => {
+    // Send an HTML response
+    res.send("<span style='display: flex;justify-content: center;align-items: center;height: 100vh; text-transform: uppercase; font-size: 4rem; font-weight: 900;background: linear-gradient(90deg,#f5f5f5,darkorange,orangered,#f9f9f9);background-clip: text;color: transparent;'>Backend is running</span>");
+});
+
+app.use('/api/contact/', require('./routes/contact'));
+
+// Function to start the server
 const startServer = async () => {
     try {
+        // Connect to the database
         await connectDB();
-        // Middleware to parse JSON requests
-        app.use(cors())
-        app.use(express.json());
-        // Routes
-
-        app.get("/", async (req, res) => {
-            //     send a html response
-            res.send("<span style='display: flex;justify-content: center;align-items: center;height: 100vh; text-transform: uppercase; font-size: 4rem; font-weight: 900;background: linear-gradient(90deg,#f5f5f5,darkorange,orangered,#f9f9f9);background-clip: text;color: transparent;'>Backend is running</span>");
-        })
-
-        app.use('/api/contact/', require('./routes/contact'));
-
         // Start the server
-        app.listen(5000, 'localhost', () => {
-            console.log('Server is running on http://localhost:5000');
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
         });
     } catch (error) {
         console.error('Failed to start server', error);
@@ -28,4 +33,5 @@ const startServer = async () => {
     }
 };
 
+// Start the server
 startServer();
