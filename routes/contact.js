3761bbe545c46bc3a2746eef35131a9ad5cd6b1e
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-19 21:44:30 +0530

    added top contacts api

--- routes/contact.js
+++ routes/contact.js
@@ -20,6 +20,25 @@ router.get('/all', async (req, res) => {
     }
 });
 
+router.get('/top', async (req, res) => {
+    try {
+        const limit = req.query.limit ? parseInt(req.query.limit) : 5;
+        const contacts = await Contact.find().sort({createdAt: -1}).limit(limit);
+        if (!contacts || contacts.length === 0) {
+            return res.status(404).json({
+                success: false, data: null, message: "No data found!"
+            });
+        }
+        res.status(200).json({
+            success: true, data: contacts, message: "Data Found!"
+        });
+    } catch (e) {
+        res.status(500).json({
+            success: false, data: null, message: `Internal Server Error ${e}`
+        });
+    }
+})
+
 router.post("/add", async (req, res) => {
     try {
         const {name, email, message} = req.body;
@@ -41,5 +60,6 @@ router.post("/add", async (req, res) => {
         console.log("Error: " + e);
     }
 });
+
 
 module.exports = router;
